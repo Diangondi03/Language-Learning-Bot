35dd584f5b70c66816cb4453689d3561e844d3ae
Diangondi03 <elguito2003@gmail.com>
2025-06-21 19:26:02 -0400

    fix: adjust footer positioning and update background gradient for improved layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <>
-        <footer className="absolute bottom-[-10vh] w-full  bg-gray-800 text-white h-[10vh] flex items-center">
+        <footer className="absolute bottom-[-10vh] md:bottom-[-9.7vh] w-full  bg-gray-800 text-white h-[10vh] flex items-center">
         <div className="container mx-auto text-center">
           <p className="text-sm">&copy; {new Date().getFullYear()} Language Learning Bot. All rights reserved.</p>
           <div className="mt-2 space-x-4">
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -6,14 +6,14 @@ function Start() {
 
   return (
     <>
-      <div className="navbar bg-indigo-950 relative h-[10vh]">
+      <div className="navbar bg-indigo-950 relative h-[10vh] m-0">
         <h1 className="font-bold text-white">Language Learning Bot</h1>
         <div className="absolute flex right-3 gap-5">
         <Link to="/auth/login" className="btn btn-accent">Log in</Link>
         <Link to="/auth/signup" className="btn btn-primary">Sign up</Link>
         </div>
       </div>
-      <div className="text-black h-[80vh] mb-[10vh] flex flex-col justify-center items-center bg-gradient-to-b from-blue-200 to-blue-500">
+      <div className="text-black h-[80vh] mb-[10vh] flex flex-col justify-center items-center bg-gradient-to-b from-blue-400 to-blue-600">
         <div className="text-center my-10">
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Language Learning Bot</h1>

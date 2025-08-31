f83d29eaad3326db1841c405afe9a306f045d017
Diangondi03 <elguito2003@gmail.com>
2025-08-30 21:53:28 -0400

    change listen message

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use('/message',authMiddleware,messageRoutes);
 app.use('/gemini',authMiddleware,geminiRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
 
 export default app;
